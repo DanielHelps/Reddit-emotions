273e6588b5cf3129004eb9daae1058f1121787d8
Daniel <mdan1000@gmail.com>
2022-05-28 23:24:43 +0300

    adding caching option for previous searches

--- LoveHateGame/train.py
+++ LoveHateGame/train.py
@@ -5,15 +5,21 @@ from main.models import SearchQ
 import datetime
 from main import emotion_check
 
+global train_day
+train_day = 0
+
 def train_today():
-    
-    pytrends = TrendReq(hl='en-US')
-    today_trends = pytrends.trending_searches()
-    date_today = datetime.datetime.date(datetime.datetime.now())
-    for item in today_trends.loc[:,0]:
-        print(item)
-        if SearchQ.objects.filter(query=item, date=date_today) is None and scores is not None:
-            scores = emotion_check.main(item)
-            search_obj = SearchQ(query=item, score=list(scores)[0], date = date_today)        
-            search_obj.save()
-        pass
+    global train_day
+    if train_day != datetime.datetime.date(datetime.datetime.now()):
+        train_day = datetime.datetime.date(datetime.datetime.now())
+        pytrends = TrendReq(hl='en-US')
+        today_trends = pytrends.trending_searches()
+        date_today = datetime.datetime.date(datetime.datetime.now())
+        for item in today_trends.loc[:,0]:
+            print(item)
+            if list(SearchQ.objects.filter(query=item, date=date_today)) == []:
+                scores = emotion_check.main(item)
+                if  scores is not None:
+                    search_obj = SearchQ(query=item, score=list(scores)[0], date = date_today)        
+                    search_obj.save()
+            pass

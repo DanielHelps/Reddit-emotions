c148f1a8f1f0bf483a86725eedc6b74167f7382e
Daniel <mdan1000@gmail.com>
2022-06-01 23:44:00 +0300

    added edge cases checks, added training section

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('search requests', views.search_requests, name='search_requests'),
     path('search=<str:query>/<int:id>', views.emotion_check_view, name='emotion check'),
+    path('search requests', views.search_requests, name='search_requests'),
 ]

25d5c354287a863692a8cbba6975e2447a5d28fe
Daniel <mdan1000@gmail.com>
2022-07-23 22:23:53 +0300

    adding twitter samples download

--- main/emotion_check.py
+++ main/emotion_check.py
@@ -24,6 +24,7 @@ def import_top_100() -> list:
     except:
         import nltk
         nltk.downloader.download('vader_lexicon')
+        nltk.download('twitter_samples')
         main_training()
         pos_100 = top_100.objects.latest('pos_date').top_obj
         neg_100 = top_100.objects.latest('neg_date').top_obj

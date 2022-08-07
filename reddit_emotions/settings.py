43bebbc9f33ee56c8fc4ef7866fa98fc9402e01e
Daniel <mdan1000@gmail.com>
2022-08-07 19:20:04 +0300

    trying again

--- reddit_emotions/settings.py
+++ reddit_emotions/settings.py
@@ -51,7 +51,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
